f5425e045fb18f640d6c838783e0cc5d5af80cf2
ClaryBess <jexzuboning@126.com>
2020-08-18 15:13:41 +0800

    editShow

--- test/src/main/java/com/example/test/controller/EditController.java
+++ test/src/main/java/com/example/test/controller/EditController.java
@@ -2,10 +2,13 @@ package com.example.test.controller;
 
 import com.example.test.bean.CommonResult;
 import com.example.test.bean.Edit;
+import com.example.test.bean.EditShow;
 import com.example.test.service.EditService;
+import com.example.test.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -13,6 +16,8 @@ public class EditController {
 
     @Autowired
     EditService editService;
+    @Autowired
+    UserService userService;
 
     @GetMapping("/edit/user/{UserID}")
     public List<Edit> getEditByUser(@PathVariable("UserID") Integer UserID){
@@ -25,8 +30,13 @@ public class EditController {
         return editService.insertEdit(edit);
     }
         @PostMapping("/edit/get/{DocID}")
-        public List<Edit> getEditList(@PathVariable Integer DocID){
-            return editService.getEditByDoc(DocID);
+        public List<EditShow> getEditList(@PathVariable Integer DocID){
+            List<Edit> edits=editService.getEditByDoc(DocID);
+            List<EditShow> editShows=new ArrayList<>();
+            for(Edit edit:edits){
+                editShows.add(new EditShow(userService.getUserById(edit.getUserID()).getUserName(),edit.getDateTime()));
+            }
+            return editShows;
         }
 
         @PostMapping("/edit/add")
