8ac16c48ecbcb0dc7254ea6f5fb3936dd76b9915
ClaryBess <jexzuboning@126.com>
2020-08-19 01:12:08 +0800

    显示加入团队

--- test/src/main/java/com/example/test/controller/TeamController.java
+++ test/src/main/java/com/example/test/controller/TeamController.java
@@ -1,8 +1,11 @@
 package com.example.test.controller;
 
+import com.example.test.bean.CommonResult;
 import com.example.test.bean.Team;
+import com.example.test.bean.TeamShow;
 import com.example.test.mapper.TeamMapper;
 import com.example.test.service.TeamService;
+import com.example.test.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.*;
@@ -10,22 +13,52 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 public class TeamController {
 
     @Autowired
     TeamService teamService;
+    @Autowired
+    UserService userService;
 
-    @PostMapping("/team/insertTeam")
-    public Team insertTeam(@RequestBody Team team){
-        return teamService.insertTeam(team);
+    @PostMapping("/team/addTeam")
+    public CommonResult addTeam(@RequestBody Team team){
+        return new CommonResult(200,null,teamService.insertTeam(team));
     }
 
     @PostMapping("/team/getTeam")
     public Team getTeam(@RequestBody Integer TeamID){
         return teamService.getTeamById(TeamID);
     }
+
+    @PostMapping("/team/inTeams")
+    public List<TeamShow> inTeams(@RequestBody Integer UserID){
+        List<Team> teams=teamService.getTeamByUser(UserID);
+        List<TeamShow> teamShows=new ArrayList<>();
+        for(Team team:teams){
+            teamShows.add(new TeamShow(team.getTeamID(),UserID,team.getTeamName(),userService.getUserById(UserID).getUserName()));
+        }
+        return teamShows;
+    }
+
+    @PostMapping("/team/quit/{TeamID}")
+    public CommonResult quit(@PathVariable("TeamID") Integer TeamID,@RequestBody Integer UserID){
+        teamService.deleteByTeamAndUser(TeamID,UserID);
+        return new CommonResult(200, null,null);
+    }
+
+    @PostMapping("/team/delete/{TeamID}")
+    public CommonResult delete(@PathVariable("TeamID")Integer TeamID){
+        teamService.deleteTeam(TeamID);
+        return new CommonResult(200,null,null);
+    }
+
+//    @PostMapping("/team/updateInfo/{TeamID}")
+//    public CommonResult updateInfo(@PathVariable)
 
 //    @PostMapping("/team/1/update/info")
 //    public Team updateTeamInfo(@RequestBody String TeamInfo, Integer userId, Integer TeamId){
