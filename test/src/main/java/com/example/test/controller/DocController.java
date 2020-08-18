8579e0c2241c7188737fb6b2bebe2886d9c59f4c
unknown <343525855@qq.com>
2020-08-18 17:08:33 +0800

    add endEdit

--- test/src/main/java/com/example/test/controller/DocController.java
+++ test/src/main/java/com/example/test/controller/DocController.java
@@ -163,6 +163,13 @@ public class DocController {
         return new CommonResult(200,null,null);
     }
 
+    @PostMapping("/doc/endEdit/{DocID}")
+    public CommonResult endEdit(@PathVariable Integer DocID){
+        Document document=docService.getDocById(DocID);
+        document.setEditable(1);
+        docService.updateEdi(document);
+        return new CommonResult(200,null,null);
+    }
 
     @RequestMapping(value = "/doc/edit",method = RequestMethod.POST)
     public CommonResult editDoc(@RequestBody Document document){
